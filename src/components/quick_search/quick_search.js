433c4100340ea5de162ca31563befeb717a4dc7a
Simon <simonlee1125@gmail.com>
2022-07-30 13:02:57 -0400

    FRendering cards based on data fetched from star wars API

--- src/components/quick_search/quick_search.js
+++ src/components/quick_search/quick_search.js
@@ -10,9 +10,70 @@ submitBtn.textContent = 'Search';
 quick_search.appendChild(submitBtn);
 
 function searchSwapi(text) {
-  alert(text);
-  // clear text
-  searchBar.value = '';
+  // alert(text);
+
+  const SWAPIbaseURL = 'https://swapi.dev/api/';
+
+  fetch(SWAPIbaseURL + text)
+    .then((response) => response.json())
+    .then((data) => {
+      const cardDisplay = document.createElement('div');
+      cardDisplay.setAttribute('class', 'card-display');
+
+      for (const result of data.results) {
+        const card = createDataCard(result);
+        cardDisplay.appendChild(card);
+      }
+
+      document.getElementById('root').appendChild(cardDisplay);
+    });
+}
+
+function createDataCard(dataObj) {
+  // console.log(dataObj);
+  const { name, height, mass, hair_color, eye_color, skin_color } = dataObj;
+
+  const card = document.createElement('div');
+  card.setAttribute('class', 'data-card');
+
+  const nameTag = document.createElement('h1');
+  nameTag.classList.add('card-name');
+  nameTag.textContent = name;
+  card.appendChild(nameTag);
+
+  const dataList = document.createElement('ul');
+  card.appendChild(dataList);
+
+  const heightTag = document.createElement('li');
+  heightTag.classList.add('card-height');
+  heightTag.textContent = `height: ${height}`;
+
+  const massTag = document.createElement('li');
+  massTag.classList.add('card-mass');
+  massTag.textContent = `mass: ${mass}`;
+
+  const hairColorTag = document.createElement('li');
+  hairColorTag.classList.add('card-hairColor');
+  hairColorTag.textContent = `hair color: ${hair_color}`;
+
+  const eyeColorTag = document.createElement('li');
+  eyeColorTag.classList.add('card-eyeColor');
+  eyeColorTag.textContent = `eye color: ${eye_color}`;
+
+  const skinColorTag = document.createElement('li');
+  skinColorTag.classList.add('card-skinColor');
+  skinColorTag.textContent = `skin color: ${skin_color}`;
+
+  const cardAttributes = [
+    heightTag,
+    massTag,
+    hairColorTag,
+    eyeColorTag,
+    skinColorTag,
+  ];
+  for (const tag of cardAttributes) dataList.appendChild(tag);
+
+  return card;
 }
 
 quick_search.addEventListener('submit', (event) => {
