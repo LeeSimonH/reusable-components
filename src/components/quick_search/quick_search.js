f58966881e2a724f816087c4e2a29b2c03d24f4b
Simon <simonlee1125@gmail.com>
2022-08-01 21:49:51 -0400

    Auto complete in progress

--- src/components/quick_search/quick_search.js
+++ src/components/quick_search/quick_search.js
@@ -1,13 +1,94 @@
+// import '../../style.scss';
+
+/**
+ * creating DOM elements, setting attributes
+ */
 const quick_search = document.createElement('form');
-quick_search.setAttribute('id', 'my-search-js');
+quick_search.setAttribute('id', 'my-search');
+quick_search.setAttribute('autocomplete', 'off');
+quick_search.setAttribute('action', '/action_page.php');
+
+/**
+ * <form id="my-search" autocomplete="off" action="/.action_page.php">
+ * </form>
+ */
+
+const autocompleteDiv = document.createElement('div');
+autocompleteDiv.setAttribute('class', 'autocomplete');
+quick_search.appendChild(autocompleteDiv);
+
+/**
+ * <form id="my-search" autocomplete="off" action="/.action_page.php">
+ *   <div class="autocomplete">
+ *   </div>
+ * </form>
+ */
+
+// const searchFilterDropdown = document.createElement('div');
+// searchFilterDropdown.setAttribute('id', 'search-filters');
+// quick_search.appendChild(searchFilterDropdown);
 
 const searchBar = document.createElement('input');
-searchBar.setAttribute('placeholder', 'Search...');
-quick_search.appendChild(searchBar);
+searchBar.setAttribute('id', 'search-input');
+searchBar.setAttribute('placeholder', 'People...');
+searchBar.setAttribute('type', 'text');
+autocompleteDiv.appendChild(searchBar);
 
-const submitBtn = document.createElement('button');
-submitBtn.textContent = 'Search';
+/**
+ * <form id="my-search" autocomplete="off" action="/.action_page.php">
+ *   <div class="autocomplete">
+ *     <input id="search-input" type="text" placeholder="People...">
+ *     </input>
+ *   </div>
+ * </form>
+ */
+
+const submitBtn = document.createElement('input');
+submitBtn.setAttribute('type', 'submit');
 quick_search.appendChild(submitBtn);
+
+/**
+ * <form id="my-search" autocomplete="off" action="/.action_page.php">
+ *   <div class="autocomplete">
+ *     <input id="search-input" type="text" placeholder="People...">
+ *   </div>
+ *   <input type="submit">
+ * </form>
+ */
+
+/**
+ * autocomplete information
+ */
+
+const peopleNames = [];
+
+const fetchPeopleData = async (url) => {
+  await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const peopleArr = data.results;
+
+      for (const { name } of peopleArr) {
+        peopleNames.push(name);
+      }
+      return data.next;
+    });
+};
+
+function getPeopleNames() {
+  let baseurl = 'https://swapi.dev/api/people';
+  fetchPeopleData(baseurl);
+  for (let page = 2; page < 9; page++) {
+    fetchPeopleData(baseurl + '/?page=' + page);
+  }
+}
+
+getPeopleNames();
+console.log('all names: ', peopleNames);
+
+/**
+ * defining functions
+ */
 
 function searchSwapi(text) {
   clearCardDisplay();
@@ -27,6 +108,106 @@ function searchSwapi(text) {
 
       document.getElementById('root').appendChild(cardDisplay);
     });
+}
+
+function autocomplete(input, arr) {
+  let currentFocus;
+
+  searchBar.addEventListener('input', function (e) {
+    let a,
+      b,
+      val = this.value;
+
+    closeAllLists();
+    if (!val) {
+      return false;
+    }
+    currentFocus = -1;
+
+    // create div element that will contain the items (values)
+    a = document.createElement('div');
+    a.setAttribute('id', this.id + 'autocomplete-list');
+    a.setAttribute('class', 'autocomplete-items');
+
+    this.parentNode.appendChild(a);
+
+    for (let i = 0; i < arr.length; i++) {
+      let subtring = arr[i].substr(0, val.length);
+      // check if item starts with same letters as the text input field value
+      if (subtring.toUpperCase() == val.toUpperCase()) {
+        // create a div for each matching element
+        b = document.createElement('div');
+        // make matching letters bold
+        b.innerHTML = '<strong>' + subtring + '</strong>';
+        b.innerHTML += arr[i].substr(val.length);
+        // insert a input field that will hold the current array item's value:
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+
+        /*execute a function when someone clicks on the item value (DIV element):*/
+        b.addEventListener('click', function (e) {
+          /*insert the value for the autocomplete text field:*/
+          input.value = this.getElementsByTagName('input')[0].value;
+          /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+          closeAllLists();
+        });
+        a.appendChild(b);
+      }
+    }
+  });
+
+  /*execute a function presses a key on the keyboard:*/
+  searchBar.addEventListener('keydown', function (e) {
+    var x = document.getElementById(this.id + 'autocomplete-list');
+    if (x) x = x.getElementsByTagName('div');
+    if (e.keyCode == 40) {
+      /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+      currentFocus++;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 38) {
+      //up
+      /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+      currentFocus--;
+      /*and and make the current item more visible:*/
+      addActive(x);
+    } else if (e.keyCode == 13) {
+      /*If the ENTER key is pressed, prevent the form from being submitted,*/
+      e.preventDefault();
+      if (currentFocus > -1) {
+        /*and simulate a click on the "active" item:*/
+        if (x) x[currentFocus].click();
+      }
+    }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = x.length - 1;
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add('autocomplete-active');
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove('autocomplete-active');
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName('autocomplete-items');
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != searchBar) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
 }
 
 function createDataCard(dataObj) {
@@ -83,10 +264,16 @@ function clearCardDisplay() {
   }
 }
 
+/**
+ * adding Event Listeners to DOM elements
+ */
+
 quick_search.addEventListener('submit', (event) => {
   event.preventDefault();
   const text = searchBar.value;
   if (text !== '') searchSwapi(text);
 });
 
-module.exports = quick_search;
+autocomplete(document.getElementById('search-input'), peopleNames);
+
+export default quick_search;
