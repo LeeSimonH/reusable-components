13e771185851f4b2ff52b9d8721cc1db882b70ff
Simon <simonlee1125@gmail.com>
2022-08-15 23:29:06 -0400

    React DynamicForm component is now fully functional.

--- src/components/dynamic form/DynamicForm.jsx
+++ src/components/dynamic form/DynamicForm.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function DynamicForm () {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [colorChoice, setColorChoice] = useState('');
+  const [submittedForms, setSubmittedForms] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    let outputStr = '';
+
+    const formData = {
+      firstName,
+      lastName,
+      colorChoice,
+      timeSubmitted: new Date().toUTCString()
+    }
+
+    setSubmittedForms(prevForms => {
+      prevForms.push(formData);
+    });
+    
+    outputStr += 'Form successfully saved! \n';
+    outputStr += `First name: ${firstName} \n`;
+    outputStr += `Last name: ${lastName} \n`;
+    outputStr += `Favorite color: ${colorChoice} \n`;
+    outputStr += `Time submitted: ${formData.timeSubmitted} \n`
+
+    alert(outputStr);
+  }
+
   return (
     <div>
-      <form id='myForm' autoComplete='off'>
+      <form id='myForm' autoComplete='off' onSubmit={e => handleSubmit(e)}>
         <label htmlFor="firstName">First name:</label>
-        <input type="text" id='fName' name='firstName' required />
+        <input type="text" id='fName' name='firstName' value={firstName} onChange={e => setFirstName(e.target.value)} required />
         <label htmlFor="lastName">Last name:</label>
-        <input type="text" id='lName' name='lastName' required />
+        <input type="text" id='lName' name='lastName' value={lastName} onChange={e => setLastName(e.target.value)} required />
         <label htmlFor="colorsDropdown">Favorite color:</label>
-        <input list='colors' name='colorDropdown' />
+        <input list='colors' name='colorDropdown' value={colorChoice} onChange={(e) => setColorChoice(e.target.value)} required />
+
         <datalist id="colors">
           <option value="red"></option>  
           <option value="blue"></option>
@@ -18,8 +48,14 @@ export default function DynamicForm () {
           <option value="orange"></option>  
           <option value="other"></option>
         </datalist>
-          
-        
+        {colorChoice == 'other' ? 
+          (<>
+            <label htmlFor="otherColor">If "other," please specify:</label>
+            <input type="text" id="otherColorInput" name="otherColor" />
+          </>)
+          : null
+        }
+        <input id="submitBtn" type='submit' />
       </form>
     </div>
   )
